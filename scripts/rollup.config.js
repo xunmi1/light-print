452ef076ac5c56e0ffa49a50562852d003508d3b
xunmi <xunmi1@outlook.com>
2020-05-07 14:54:37 +0800

    build: change build target to ES5, release 1.1.0

--- scripts/rollup.config.js
+++ scripts/rollup.config.js
@@ -1,13 +1,13 @@
 import json from '@rollup/plugin-json';
-import sucrase from '@rollup/plugin-sucrase';
 import resolve from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import pkg from '../package.json';
 
+const RUNTIME_CONTEXT = 'window';
 
 const currentYear = new Date().getFullYear();
 const banner =
-`/*!
+  `/*!
  * ${pkg.name} v${pkg.version}
  * (c) ${currentYear > 2020 ? '2020-' : ''}${currentYear} ${pkg.author}
  * Released under the MIT License.
@@ -21,27 +21,18 @@ const outputFileList = [
   { format: 'esm', min: true },
 ];
 
-const output = outputFileList.map(config => {
-  const file = `dist/${pkg.name}.${config.format}${config.min ? '.min' : ''}.js`;
-  return { name: config.name, format: config.format, banner, file, sourcemap: !config.min };
+const output = outputFileList.map(({ name, format, min }) => {
+  const file = `dist/${pkg.name}.${format}${min ? '.min' : ''}.js`;
+  const plugins = min ? [terser()] : [];
+  return { name, format, banner, file, sourcemap: false, plugins };
 });
 
-const terserOptions = {
-  include: [/^.+\.min\.js$/],
-  output: { comments: `/${pkg.name} v${pkg.version}/` },
-};
-
 export default {
-  input: 'src/index.ts',
+  context: RUNTIME_CONTEXT,
   output,
   plugins: [
     json({ namedExports: false }),
-    resolve({ extensions: ['.js', '.ts'] }),
-    sucrase({
-      exclude: ['node_modules/**'],
-      transforms: ['typescript']
-    }),
-    terser(terserOptions),
+    resolve(),
   ],
 };
 
